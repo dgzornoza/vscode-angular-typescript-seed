bc3560bbcf83b9362c00305bf438d529412d5fd5
NUNSYS\gonzalez.zornoza <gonzalez.zornoza@nunsys.com>
2016-09-12 09:11:40 +0200

    --

--- src/components/statusBar.ts
+++ src/components/statusBar.ts
@@ -4,6 +4,62 @@ import "reflect-metadata";
 
 import { IDisposable } from "./../models/interfaces/common";
 
+
+// --------------------------------------------
+// StatusBar items
+// --------------------------------------------
+
+abstract class IStatusBarItem implements IDisposable {
+
+    protected _statusItem: vsc.StatusBarItem;
+    protected _disposable:  vsc.Disposable;
+
+    public dispose(): void {
+        this._statusItem.dispose();
+        this._disposable.dispose();
+    }
+}
+
+//@injectable()
+export class StatusBarChangeViewItem extends IStatusBarItem {
+
+
+
+        constructor() {
+            super();
+
+            // Create as needed
+            if (!this._statusItem) {
+                this._statusItem = vsc.window.createStatusBarItem(vsc.StatusBarAlignment.Left);
+                this._statusItem.text = "$(file-code)";
+                this._statusItem.tooltip = "Cambiar Vista/controlador";
+                this._statusItem.show();
+            }
+
+            // vsc.window.onDidChangeActiveTextEditor(this._onDidChangeActiveTextEditor);
+
+
+            // // subscribe to selection change and editor activation events
+            // let subscriptions: vsc.Disposable[] = [];
+            // vsc.window.onDidChangeActiveTextEditor(this._onDidChangeActiveTextEditor, this, subscriptions);
+
+            // // create a combined disposable from both event subscriptions
+            // this._disposable = vsc.Disposable.from(...subscriptions);
+        }
+
+        public dispose(): void {
+            let a: number = 5;
+            let b: number = a/5;
+        }
+
+}
+
+
+// --------------------------------------------
+// StatusBar
+// --------------------------------------------
+
+
 export interface IStatusBar extends IDisposable {
 
 }
@@ -11,53 +67,19 @@ export interface IStatusBar extends IDisposable {
 @injectable()
 export class StatusBar implements IStatusBar  {
 
-    private _statusBarChangeViewController: vsc.StatusBarItem;
-    private _disposable:  vsc.Disposable;
+    private _statusBarChangeViewItem: StatusBarChangeViewItem;
 
+    // constructor(@inject("StatusBarChangeViewItem") statusBarChangeViewItem: StatusBarChangeViewItem) {
+    //     this._statusBarChangeViewItem = statusBarChangeViewItem;
+    // }
     constructor() {
 
-        // Create as needed
-        if (!this._statusBarChangeViewController) {
-            this._statusBarChangeViewController = vsc.window.createStatusBarItem(vsc.StatusBarAlignment.Left);
-            this._statusBarChangeViewController.text = "$(file-code)";
-            this._statusBarChangeViewController.tooltip = "Cambiar Vista/controlador";
-            this._statusBarChangeViewController.show();
-        }
-
-        vsc.window.onDidChangeActiveTextEditor(this._onDidChangeActiveTextEditor);
-
-
-        // subscribe to selection change and editor activation events
-        let subscriptions: vsc.Disposable[] = [];
-        vsc.window.onDidChangeActiveTextEditor(this._onDidChangeActiveTextEditor, this, subscriptions);
-
-        // create a combined disposable from both event subscriptions
-        this._disposable = vsc.Disposable.from(...subscriptions);
     }
 
     public dispose(): void {
-        this._statusBarChangeViewController.dispose();
-        this._disposable.dispose();
+        let a: number = 5;
+        let b: number = a / 5;
     }
 
-    // private _onDidChangeActiveTextEditor(): void {
-
-    //     // Get the current text editor
-    //     let editor: vsc.TextEditor = vsc.window.activeTextEditor;
-    //     if (!editor) {
-    //         this._statusBarChangeViewController.hide();
-    //         return;
-    //     }
-
-    //     let doc: vsc.TextDocument = editor.document;
-
-    //     // Only update status if an typescript/html file
-    //     if (doc.languageId === "typescript" || doc.languageId === "html") {
-    //         this._statusBarChangeViewController.show();
-    //     } else {
-    //         this._statusBarChangeViewController.hide();
-    //     }
-
-    // }
-
 }
+
