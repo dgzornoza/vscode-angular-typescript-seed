51289c10c15a4c66c5c10e799afd94a37ed95cb1
NUNSYS\gonzalez.zornoza <gonzalez.zornoza@nunsys.com>
2016-09-12 09:22:53 +0200

    commented code

--- src/commands/changeViewControllerCmd.ts
+++ src/commands/changeViewControllerCmd.ts
@@ -13,21 +13,21 @@ export class ChangeViewControllerCmd extends ICommand<any> {
 
     public execute(...params: any[]): void {
 
-        // Get the current text editor
-        let editor: vsc.TextEditor = vsc.window.activeTextEditor;
-        if (!editor) {
-            this._statusBarChangeViewController.hide();
-            return;
-        }
+        // // Get the current text editor
+        // let editor: vsc.TextEditor = vsc.window.activeTextEditor;
+        // if (!editor) {
+        //     this._statusBarChangeViewController.hide();
+        //     return;
+        // }
 
-        let doc: vsc.TextDocument = editor.document;
+        // let doc: vsc.TextDocument = editor.document;
 
-        // Only update status if an typescript/html file
-        if (doc.languageId === "typescript" || doc.languageId === "html") {
-            this._statusBarChangeViewController.show();
-        } else {
-            this._statusBarChangeViewController.hide();
-        }
+        // // Only update status if an typescript/html file
+        // if (doc.languageId === "typescript" || doc.languageId === "html") {
+        //     this._statusBarChangeViewController.show();
+        // } else {
+        //     this._statusBarChangeViewController.hide();
+        // }
 
     }
 
